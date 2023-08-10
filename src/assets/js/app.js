8f204753fcfcb5332534ec02324cc00bedfd2d08
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-08-10 17:11:33 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,43 +1,51 @@
-const range = document.querySelector("[data-range]");
+const slider = document.querySelector("[data-range]");
 const pageviews = document.querySelector("[data-pageviews]");
 const price = document.querySelector("[data-price]");
-const toggleButton = document.querySelector("[date-toggle]");
+const toggleButton = document.getElementById("toggle");
 
-range.addEventListener("input", (e) => {
-  const rangeValue = e.target.value;
-  const progress = (rangeValue / range.max) * 100;
+slider.oninput = function () {
+  const rangeValue = slider.value;
+  const progress = (rangeValue / slider.max) * 100;
 
-  range.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${progress}%, hsl(224, 65%, 95%) ${progress}%)`;
+  slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${progress}%, hsl(224, 65%, 95%) ${progress}%)`;
+
+  function togglePrice(amount) {
+    price.textContent = `$${amount}.00`;
+    toggleButton.checked
+      ? (price.textContent = `$${amount - amount * 0.25}.00`)
+      : (price.textContent = `$${amount}.00`);
+  }
+
+  function togglePageViews(view) {
+    return slider.value >= 36
+      ? (pageviews.textContent = `${view}M`)
+      : (pageviews.textContent = `${view}K`);
+  }
 
   switch (rangeValue) {
     case "8":
-      pageviews.textContent = "10K";
-      price.textContent = "8.00";
+      togglePageViews(10);
+      togglePrice(8);
       break;
     case "12":
-      pageviews.textContent = "50K";
-      price.textContent = "12.00";
+      togglePageViews(50);
+      togglePrice(8);
       break;
     case "16":
-      pageviews.textContent = "100K";
-      price.textContent = "16.00";
+      togglePageViews(100);
+      togglePrice(16);
       break;
     case "24":
-      pageviews.textContent = "500K";
-      price.textContent = "24.00";
+      togglePageViews(500);
+      togglePrice(24);
       break;
     case "36":
-      pageviews.textContent = "1M";
-      price.textContent = "36.00";
+      togglePageViews(1);
+      togglePrice(36);
       break;
   }
-});
+};
 
 toggleButton.addEventListener("input", () => {
-  if (toggleButton.checked) {
-    price.textContent = (
-      price.textContent -
-      parseInt(price.textContent) * 0.25
-    ).toFixed(2);
-  }
+  slider.oninput();
 });
